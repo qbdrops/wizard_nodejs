ad7f377882c28b73f3d94b4b1eea1580ae13b88e
Juin Chiu <juin.chiu@infinitechain.io>
2018-02-13 17:41:42 +0800

    Fix console script

--- console.js
+++ console.js
@@ -7,13 +7,13 @@ var ctx = repl.start({
   output: process.stdout
 }).context;
 
-var bundleFile = path.resolve('./dist/infinitechain_sdk.js');
+var bundleFile = path.resolve('./dist/infinitechain_nodejs');
 
-var IFCBuilder = require('./dist/infinitechain_sdk.js');
+var IFCBuilder = require('./dist/infinitechain_nodejs');
 ctx.IFCBuilder = IFCBuilder;
 
-chokidar.watch('./dist/infinitechain_sdk.js', {ignored: /[\/\\]\./}).on('change', function(event, path) {
+chokidar.watch('./dist/infinitechain_nodejs', {ignored: /[\/\\]\./}).on('change', function(event, path) {
   delete require.cache[bundleFile];
-  var IFCBuilder = require('./dist/infinitechain_sdk.js');
+  var IFCBuilder = require('./dist/infinitechain_nodejs');
   ctx.IFCBuilder = IFCBuilder;
 });
