2bbd6f1d16da6041e3a170d6cdff3a82da65eac1
Jerry.Jheng <jerry.jheng@infinitechain.io>
2018-02-14 11:37:30 +0800

    Fix importPrivateKey and encrypt function.

--- src/cryptos/cipher.js
+++ src/cryptos/cipher.js
@@ -3,50 +3,55 @@ import constants from 'constants';
 import assert from 'assert';
 
 class Cipher {
-  constructor (rsaPrivateKey) {
-    this.privateKey = rsaPrivateKey;
+  constructor () {
+    this.privateKey = null;
     this.publicKey = null;
-    this.key = null;
   }
 
   getOrNewKeyPair = () => {
-    if (!this.key) {
-      this.key = new NodeRSA({ encryptionScheme: { scheme: 'pkcs1', padding: constants.RSA_NO_PADDING } }).generateKeyPair(2048);
+    if (!this.privateKey) {
+      let key = new NodeRSA({ encryptionScheme: { scheme: 'pkcs1', padding: constants.RSA_NO_PADDING } }).generateKeyPair(2048);
+      this.publicKey = key.exportKey('public');
+      this.privateKey = key.exportKey('private');
     }
 
-    return this.key.exportKey('private');
+    return this.privateKey;
   }
 
   importPrivateKey = (privateKey) => {
-    this.key = new NodeRSA(privateKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
+    let key = new NodeRSA(privateKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
+    this.publicKey = key.exportKey('public');
+    this.privateKey = key.exportKey('private');
   }
 
   encrypt = (message, publicKey = null) => {
-    assert(publicKey || this.key, 'Please do getOrNewKeyPair() first.');
+    assert(publicKey || this.publicKey, 'Please do getOrNewKeyPair() first.');
     if (publicKey) {
-      let key = new NodeRSA(publicKey, {encryptionScheme:{scheme:'pkcs1', padding: constants.RSA_NO_PADDING}});
+      let key = new NodeRSA(publicKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
       return key.encrypt(message, 'base64');
     } else {
-      return this.key.encrypt(message, 'base64');
+      let key = new NodeRSA(this.publicKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
+      return key.encrypt(message, 'base64');
     }
   }
 
   decrypt = (cipherMessage, privateKey = null) => {
-    assert(privateKey || this.key, 'Please do getOrNewKeyPair() first.');
+    assert(privateKey || this.privateKey, 'Please do getOrNewKeyPair() first.');
     if (privateKey) {
-      let key = new NodeRSA(privateKey, {encryptionScheme:{scheme:'pkcs1', padding: constants.RSA_NO_PADDING}});
+      let key = new NodeRSA(privateKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
       return key.decrypt(cipherMessage, 'utf8');
     } else {
-      return this.key.decrypt(cipherMessage, 'utf8');
+      let key = new NodeRSA(this.privateKey, { encryptionScheme: { scheme:'pkcs1', padding: constants.RSA_NO_PADDING } });
+      return key.decrypt(cipherMessage, 'utf8');
     }
   }
 
   getPrivateKey () {
-    return this.key.exportKey('private');
+    return this.privateKey;
   }
 
   getPublicKey () {
-    return this.key.exportKey('public');
+    return this.publicKey;
   }
 }
 
