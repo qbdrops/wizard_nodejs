a8a53217eaea70f275b0d132c46cfb9a9d2d8286
BenjaminLu <coopermilk123@gmail.com>
2018-04-11 17:07:59 +0800

    Add mock library for unit test.

--- test/test-ifc-builder.js
+++ test/test-ifc-builder.js
@@ -1,6 +1,11 @@
 import assert from 'assert';
 import IFCBuilder from '@/ifc-builder';
 import IFC from '@/ifc';
+import nock from 'nock';
+
+nock('http://localhost:3000')
+  .get('/contract/address')
+  .reply(200, {address: '0x68c34a54ec562b2b6efc8e61c54f9314b93b1a44'});
 
 describe('IFCBuilder', () => {
   describe('#constructor', () => {
