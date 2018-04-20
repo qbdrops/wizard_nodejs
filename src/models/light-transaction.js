076b763dca60d9eddc741d5914855fb6979fab82
Juin Chiu <juin.chiu@infinitechain.io>
2018-04-20 16:32:05 +0800

    Improve models

--- src/models/light-transaction.js
+++ src/models/light-transaction.js
@@ -1,8 +1,10 @@
 import EthUtils from 'ethereumjs-util';
 import assert from 'assert';
+import types from '@/models/types';
 
 const allowedLightTxDataKeys = ['from', 'to', 'value', 'fee', 'LSN', 'stageHeight'];
 const allowedSigKeys = ['clientLightTx', 'serverLightTx'];
+const instantWithdrawalLimit = 10;
 
 class LightTransaction {
   constructor (lightTxData, sig = null) {
@@ -41,9 +43,98 @@ class LightTransaction {
       assert(isSigFormatCorrect || isSigDefault, '\'sig\' does not have correct format.');
     });
 
-    this.lightTxData = orderedLightTxData;
+    this.lightTxData = this._normalize(orderedLightTxData);
     this.lightTxHash = EthUtils.sha3(JSON.stringify(this.lightTxData)).toString('hex');
     this.sig = sig;
+  }
+
+  _normalize = (lightTxData) => {
+    lightTxData.from        = lightTxData.from.padStart(64, '0').slice(-64);
+    lightTxData.to          = lightTxData.to.padStart(64, '0').slice(-64);
+    lightTxData.stageHeight = this._to32BytesHex(lightTxData.stageHeight, false);
+    lightTxData.LSN         = this._to32BytesHex(lightTxData.LSN, false);
+    lightTxData.value       = this._to32BytesHex(lightTxData.value, true);
+    lightTxData.fee         = this._to32BytesHex(lightTxData.fee, true);
+    return lightTxData;
+  }
+
+  _to32BytesHex = (n, toWei) => {
+    let startWith0x = ((n.toString().slice(0, 2) == '0x') && (n.toString().substring(2).length == 64));
+    let lengthIs64Bytes = (n.toString().length == 64);
+
+    if (startWith0x || lengthIs64Bytes) {
+      n = n.slice(-64);
+    } else {
+      let m = parseFloat(n);
+      m = toWei ? (m * 1e18) : m;
+      let h = m.toString(16);
+      assert(h != 'NaN', '\'' + n + '\' can not be parsed to an integer.');
+      n = h.padStart(64, '0');
+    }
+
+    return n;
+  }
+
+  type = () => {
+    let res;
+    let from = this.lightTxData.from;
+    let to = this.lightTxData.to;
+    let value = parseInt(this.lightTxData.value, 16) / 1e18;
+
+    if (from == 0 || to == 0) {
+      if (from == 0) {
+        res = types.deposit;
+      } else {
+        res = (value > instantWithdrawalLimit) ? types.withdrawal : types.instantWithdrawal;
+      }
+    } else {
+      res = types.remittance;
+    }
+
+    return res;
+  }
+
+  hasClientLightTxSig = () => {
+    return (Object.keys(this.sig.clientLightTx).sort().toString() == 'r,s,v');
+  }
+
+  hasServerLightTxSig = () => {
+    return (Object.keys(this.sig.serverLightTx).sort().toString() == 'r,s,v');
+  }
+
+  toJson = () => {
+    let json = {
+      lightTxHash: this.lightTxHash,
+      lightTxData: this.lightTxData,
+      sig: this.sig
+    };
+    return json;
+  }
+
+  toString = () => {
+    return JSON.stringify(this.toJson());
+  }
+
+  static parseProposeDeposit = (eventData) => {
+    let lightTxData = {
+      from: '0',
+      to: eventData._client,
+      value: eventData._value,
+      LSN: eventData._lsn,
+      fee: eventData._fee,
+      stageHeight: eventData._stageHeight
+    };
+
+    let sig = {
+      clientLightTx: {
+        v: eventData._v,
+        r: eventData._r,
+        s: eventData._s
+      }
+    };
+
+    let lightTx = new LightTransaction(lightTxData, sig);
+    return lightTx;
   }
 }
 
--- src/models/receipt.js
+++ src/models/receipt.js
@@ -2,28 +2,44 @@ import EthUtils from 'ethereumjs-util';
 import assert from 'assert';
 import LightTransaction from '@/models/light-transaction';
 
+const allowedReceiptJsonKeys = ['lightTxHash', 'lightTxData', 'sig', 'receiptData'];
 const allowedReceiptDataKeys = ['GSN', 'lightTxHash', 'fromBalance', 'toBalance'];
 
 class Receipt {
-  constructor (lightTx, receiptData) {
-    // check if lightTx instanceof LightTransaction object or not.
-    assert(lightTx instanceof LightTransaction, 'Parameter \'lightTx\' is not a LightTransaction instance.');
-    // check if clientLightTx and serverLightTx are empty or not.
-    assert(!(Object.keys(lightTx.sig.clientLightTx).toString() == [].toString()), '\'clientLightTx\' signature is empty.');
-    assert(!(Object.keys(lightTx.sig.serverLightTx).toString() == [].toString()), '\'serverLightTx\' signature is empty.');
-    Object.keys(receiptData).forEach(key => {
-      if (!allowedReceiptDataKeys.includes(key)) {
-        delete receiptData[key];
+  constructor (receiptJson) {
+    // Remove keys which are not in the whitelist
+    Object.keys(receiptJson).forEach(key => {
+      if (!allowedReceiptJsonKeys.includes(key)) {
+        delete receiptJson[key];
       }
     });
+
+    Object.keys(receiptJson.receiptData).forEach(key => {
+      if (!allowedReceiptDataKeys.includes(key)) {
+        delete receiptJson.receiptData[key];
+      }
+    });
+
+    // Check Json format
+    allowedReceiptJsonKeys.forEach(key => {
+      assert(Object.keys(receiptJson).includes(key), 'Parameter \'receiptJson\' does not include key \'' + key + '\'.');
+    });
+
+    // Check lightTxData format
+    let lightTx = new LightTransaction(receiptJson.lightTxData, receiptJson.sig);
+
+    // check if clientLightTx and serverLightTx are empty or not.
+    assert(lightTx.hasClientLightTxSig(), '\'clientLightTx\' signature is empty.');
+    assert(lightTx.hasServerLightTxSig(), '\'serverLightTx\' signature is empty.');
+
     // Meanwhile make an ordered receiptData
-    let receiptKeys = Object.keys(receiptData);
+    let receiptKeys = Object.keys(receiptJson.receiptData);
     let orderedReceiptData = {};
     allowedReceiptDataKeys.forEach(key => {
       assert(receiptKeys.includes(key), 'Parameter \'receiptData\' does not include key \'' + key + '\'.');
-      orderedReceiptData[key] = receiptData[key];
+      orderedReceiptData[key] = receiptJson.receiptData[key];
     });
-    assert(lightTx.lightTxHash === receiptData.lightTxHash, 'The \'lightTxHash\' is different in receiptData and lightTransaction.');
+    assert(lightTx.lightTxHash === receiptJson.receiptData.lightTxHash, 'The \'lightTxHash\' is different in receiptData and lightTransaction.');
 
     this.lightTxHash = lightTx.lightTxHash;
     this.receiptHash = EthUtils.sha3(JSON.stringify(this.receiptData)).toString('hex');
