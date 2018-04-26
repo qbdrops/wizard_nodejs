a92b7fb5bc21fc66c87e8c09ed68adf2ac7e8479
BenjaminLu <coopermilk123@gmail.com>
2018-04-26 16:03:26 +0800

    Export Infinitechain types

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var InfinitechainBuilder = require('@/infinitechain-builder').default;
 var LightTransaction = require('@/models/light-transaction').default;
 var Receipt = require('@/models/receipt').default;
+var Types = require('@/models/types').default;
 
 module.exports = {
   InfinitechainBuilder: InfinitechainBuilder,
   LightTransaction: LightTransaction,
-  Receipt: Receipt
+  Receipt: Receipt,
+  Types: Types
 };
