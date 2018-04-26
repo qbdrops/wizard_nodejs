14beaee1b2d183eafe29ba6452a4ebfbaed2d753
Juin Chiu <juin.chiu@infinitechain.io>
2018-04-26 14:15:36 +0800

    Improve receipt

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,9 +1,11 @@
 import EthUtils from 'ethereumjs-util';
 import assert from 'assert';
 import LightTransaction from '@/models/light-transaction';
+import types from '@/models/types';
 
 const allowedReceiptJsonKeys = ['lightTxHash', 'lightTxData', 'sig', 'receiptData'];
 const allowedReceiptDataKeys = ['GSN', 'lightTxHash', 'fromBalance', 'toBalance'];
+const instantWithdrawalLimit = 10;
 
 class Receipt {
   constructor (receiptJson) {
@@ -43,7 +45,7 @@ class Receipt {
 
     this.lightTxHash = lightTx.lightTxHash;
     this.lightTxData = lightTx.lightTxData;
-    this.receiptData = orderedReceiptData;
+    this.receiptData = this._normalize(orderedReceiptData);
     this.receiptHash = EthUtils.sha3(
       Object.values(this.receiptData).reduce((acc, curr) => acc + curr, '')
     ).toString('hex');
@@ -51,7 +53,45 @@ class Receipt {
     this.sig.serverReceipt = {};
   }
 
-  toJson () {
+  _normalize = (receiptData) => {
+    receiptData.GSN         = receiptData.GSN.toString(16).padStart(64, '0').slice(-64);
+    receiptData.fromBalance = receiptData.fromBalance.toString(16).padStart(64, '0').slice(-64);
+    receiptData.toBalance   = receiptData.toBalance.toString(16).padStart(64, '0').slice(-64);
+    return receiptData;
+  }
+
+  type = () => {
+    let res;
+    let from = this.lightTxData.from;
+    let to = this.lightTxData.to;
+    let value = parseInt(this.lightTxData.value, 16) / 1e18;
+
+    if (from == 0 || to == 0) {
+      if (from == 0) {
+        res = types.deposit;
+      } else {
+        res = (value > instantWithdrawalLimit) ? types.withdrawal : types.instantWithdrawal;
+      }
+    } else {
+      res = types.remittance;
+    }
+
+    return res;
+  }
+
+  hasClientLightTxSig = () => {
+    return (Object.keys(this.sig.clientLightTx).sort().toString() == 'r,s,v');
+  }
+
+  hasServerLightTxSig = () => {
+    return (Object.keys(this.sig.serverLightTx).sort().toString() == 'r,s,v');
+  }
+
+  hasServerReceiptSig = () => {
+    return (Object.keys(this.sig.serverReceipt).sort().toString() == 'r,s,v');
+  }
+
+  toJson = () => {
     let json = {
       lightTxHash: this.lightTxHash,
       lightTxData: this.lightTxData,
