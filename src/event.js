be8af3f700bd2758cb3493081b2953d9b9bb53b6
Juin Chiu <juin.chiu@infinitechain.io>
2018-05-17 14:47:40 +0800

    Remove deprecated logic from event module

--- src/event.js
+++ src/event.js
@@ -28,43 +28,7 @@ class Event {
     let sidechain = this._infinitechain.contract.sidechain();
     sidechain.VerifyReceipt({ _type: types.deposit }, this._eventOpt).watch(async (err, result) => {
       if (err) { console.trace; }
-
-      // Save receipt
-      let targetLightTxHash = result.args._lightTxHash.substring(2);
-      let lightTx = await this._infinitechain.client.getLightTx(targetLightTxHash);
-      let clientLightTxSig = lightTx.sig.clientLightTx;
-      let serverLightTxSig = {
-        v: result.args._sig_lightTx[0],
-        r: result.args._sig_lightTx[1],
-        s: result.args._sig_lightTx[2],
-      };
-      let serverReceiptSig = {
-        v: result.args._sig_receipt[0],
-        r: result.args._sig_receipt[1],
-        s: result.args._sig_receipt[2],
-      };
-
-      let receiptJson = {
-        lightTxHash: lightTx.lightTxHash,
-        lightTxData: lightTx.lightTxData,
-        sig: {
-          clientLightTx: clientLightTxSig,
-          serverLightTx: serverLightTxSig,
-          serverReceipt: serverReceiptSig,
-        },
-        receiptData: {
-          stageHeight: result.args._stageHeight,
-          GSN: result.args._gsn,
-          lightTxHash: result.args._lightTxHash.substring(2),
-          fromBalance: result.args._fromBalance,
-          toBalance: result.args._toBalance
-        }
-      };
-
-      let receipt = new Receipt(receiptJson);
-      await this._infinitechain.client.saveReceipt(receipt);
-
-      cb(err, receipt);
+      cb(err, result);
     });
   }
 
@@ -72,43 +36,7 @@ class Event {
     let sidechain = this._infinitechain.contract.sidechain();
     sidechain.VerifyReceipt({ _type: types.withdrawal }, this._eventOpt).watch(async (err, result) => {
       if (err) { console.trace; }
-
-      // Save receipt
-      let targetLightTxHash = result.args._lightTxHash.substring(2);
-      let lightTx = await this._infinitechain.client.getLightTx(targetLightTxHash);
-      let clientLightTxSig = lightTx.sig.clientLightTx;
-      let serverLightTxSig = {
-        v: result.args._sig_lightTx[0],
-        r: result.args._sig_lightTx[1],
-        s: result.args._sig_lightTx[2],
-      };
-      let serverReceiptSig = {
-        v: result.args._sig_receipt[0],
-        r: result.args._sig_receipt[1],
-        s: result.args._sig_receipt[2],
-      };
-
-      let receiptJson = {
-        lightTxHash: lightTx.lightTxHash,
-        lightTxData: lightTx.lightTxData,
-        sig: {
-          clientLightTx: clientLightTxSig,
-          serverLightTx: serverLightTxSig,
-          serverReceipt: serverReceiptSig,
-        },
-        receiptData: {
-          stageHeight: result.args._stageHeight,
-          GSN: result.args._gsn,
-          lightTxHash: result.args._lightTxHash.substring(2),
-          fromBalance: result.args._fromBalance,
-          toBalance: result.args._toBalance
-        }
-      };
-
-      let receipt = new Receipt(receiptJson);
-      await this._infinitechain.client.saveReceipt(receipt);
-
-      cb(err, receipt);
+      cb(err, result);
     });
   }
 
